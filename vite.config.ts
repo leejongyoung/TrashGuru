0d13d68a9fc12c073367a001d2dac7b285a24250
Lee Jongyoung <leejongyoung@icloud.com>
2025-12-12 20:24:17 +0900

    feat(pwa): Set up PWA and home screen icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/TrashGuru/',
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.svg'],
+      manifest: {
+        name: '쓰레기박사',
+        short_name: '쓰레기박사',
+        description: 'AI 기반 쓰레기 분리수거 전문가, 쓰레기박사',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/TrashGuru/',
+        start_url: '/TrashGuru/',
+        icons: [
+          {
+            src: 'assets/logo.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
